6390d46c6f6e4f176026cd61a873b501cfcf65ce
Default <samiv360@gmail.com>
2024-06-16 20:37:10 +0300

    Add project files.

---
+++ Threads/FileCopier.cs
@@ -0,0 +1,102 @@
+﻿namespace Threads
+{
+    class FileCopier
+    {
+        private string source;
+        private string target;
+
+        private Queue<string> operationQueueForProgress = new Queue<string>();
+        private Queue<string> operationQueueForReaderWriter = new Queue<string>();
+
+        private bool _complete = false;
+
+        public FileCopier(string source, string target)
+        {
+            this.source = source;
+            this.target = target;
+        }
+
+        public void Start()
+        {
+#if DEV
+            //// empty the target folder everytime, easier for development
+            //Console.WriteLine($"Clearing {target} dir");
+            //DirectoryInfo di = new DirectoryInfo(target);
+
+            //foreach (FileInfo file in di.GetFiles())
+            //{
+            //    file.Delete();
+            //}
+#endif
+
+            Thread reader = new Thread(new ThreadStart(Reader));
+            reader.Start();
+
+            Thread progress = new Thread(new ThreadStart(ProgressWriter));
+            progress.Start();
+
+            Thread copier = new Thread(new ThreadStart(Copier));
+            copier.Start();
+
+
+        }
+
+        private void ProgressWriter()
+        {
+            Console.WriteLine("ProgressWriter-thread starts");
+            while (_complete == false)
+            {
+                if (operationQueueForProgress.Count > 0)
+                {
+                    string s = "";
+                    lock (operationQueueForProgress)
+                    {
+                        s = operationQueueForProgress.Dequeue();
+                    }
+                    Console.WriteLine(s);
+                }
+            }
+        }
+
+        private void Reader()
+        {
+            Console.WriteLine("ReaderWriter-thread starts");
+
+            string[] files = Directory.GetFiles(source);
+
+            foreach (string file in files)
+            {
+                lock (operationQueueForProgress)
+                {
+                    operationQueueForProgress.Enqueue(file);
+                }
+
+                lock (operationQueueForReaderWriter)
+                {
+                    operationQueueForReaderWriter.Enqueue(file);
+                }
+            }
+
+            Console.WriteLine(operationQueueForProgress.Count);
+            _complete = true;
+        }
+
+        private void Copier()
+        {
+            while (_complete == false)
+            {
+                if (operationQueueForReaderWriter.Count > 0)
+                {
+                    lock (operationQueueForReaderWriter)
+                    {
+                        string val = operationQueueForReaderWriter.Dequeue();
+
+                        Console.WriteLine(val);
+
+                        File.Copy(val, Path.Combine(target, Path.GetFileName(val)), true);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Threads/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Threads;
+
+#if DEV
+string source = @"C:\Users\samiv\Documents\opiskelu\ohjelmointi\fold-1";
+string target = @"C:\Users\samiv\Documents\opiskelu\ohjelmointi\fold-2";
+#else
+
+Console.WriteLine(args);
+
+if(args.Length != 2)
+{
+    Console.WriteLine("Usage:");
+    Console.WriteLine("copier 'source' 'target'");
+    Console.WriteLine("arg 1. = source folder");
+    Console.WriteLine("arg 2. = target folder");
+}
+
+string source = args[0];
+string target = args[1];
+
+#endif
+
+FileCopier copier = new FileCopier(source, target);
+
+copier.Start();
